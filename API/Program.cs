584b253702bbdbced06ecf023538cba61858d24e
Nate Jones <15nate.jones@gmail.com>
2023-05-14 11:23:04 -0400

    Api Controller, DB

--- API/Program.cs
+++ API/Program.cs
@@ -34,4 +34,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<Persistence.DataContext>();
+    await context.Database.MigrateAsync();
+    //
+    await Persistence.Seed.SeedData(context);
+}
+catch (Exception ex)
+{
+
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
+
 app.Run();
